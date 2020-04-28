7a77b2e745bc399e5dd8a9e33765a62e33a8ed3f
ZehraDede <64098150+ZehraDede@users.noreply.github.com>
2020-04-28 16:34:08 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,7 +4,7 @@ public class HappyBirthday {
 
     public static void main(String[] args) {
         
-        System.out.println("Happy Birthday B18");
+      
         System.out.println("Happy Birthday B18 again, this line is edit from GitHub");
 
     }
